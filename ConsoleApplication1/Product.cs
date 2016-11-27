fe2c8ca490ddab0cf018639d85794bc326ea71db
darrenscrine <darrenscrine@gmail.com>
2016-11-27 21:17:08 +0000

    just playing

---
+++ ConsoleApplication1/Product.cs
@@ -0,0 +1,37 @@
+﻿using LINQtoCSV;
+
+namespace ConsoleApplication1
+{
+    class Product
+    {
+        [CsvColumn(Name = "Id", FieldIndex = 1, CanBeNull = false)]
+        public int Id { get; set; }
+
+        [CsvColumn(Name = "Name", FieldIndex = 2)]
+        public string Name { get; set; }
+
+        [CsvColumn(Name = "Author", FieldIndex = 3)]
+        public string Author { get; set; }
+
+        [CsvColumn(Name = "Stock", FieldIndex = 4)]
+        public int Stock { get; set; }
+
+        [CsvColumn(Name = "Price1", FieldIndex = 5, CanBeNull = false, OutputFormat = "C")]
+        public decimal Price1 { get; set; }
+
+        [CsvColumn(Name = "Price2", FieldIndex = 6, CanBeNull = false, OutputFormat = "C")]
+        public decimal Price2 { get; set; }
+
+        [CsvColumn(Name = "Price3", FieldIndex = 7, CanBeNull = false, OutputFormat = "C")]
+        public decimal Price3 { get; set; }
+
+        [CsvColumn(Name = "Region", FieldIndex = 8)]
+        public string Region { get; set; }
+
+        [CsvColumn(Name = "Category", FieldIndex = 9)]
+        public string Category { get; set; }
+
+        [CsvColumn(Name = "Percentage", FieldIndex = 10)]
+        public decimal Percentage { get; set; }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using LINQtoCSV;
+using System.Linq;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filepath = @"C:\Users\Darren\Downloads\SampleCSVFile_119kb.csv";
+
+            CsvFileDescription inputFileDescription = new CsvFileDescription
+            {
+                SeparatorChar = ',',
+                UseFieldIndexForReadingData = true,
+                IgnoreUnknownColumns = true,
+                EnforceCsvColumnAttribute = false
+            };
+
+            CsvContext cc = new CsvContext();
+
+            IEnumerable<Product> products = cc.Read<Product>(filepath, inputFileDescription);
+
+            var productsByName = products
+                .Where(p => p.Id < 100)
+                .Take(5);
+
+            foreach (var product in productsByName)
+            {
+                Console.WriteLine($"ID: {product.Id} : Name: {product.Name} : Price: {product.Price1}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
